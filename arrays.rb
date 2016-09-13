c8ec294f7ac3bb6cf3fb3d4d60fe0ebfe97df5c1
William Patrick O Brien <obrienwp@dcsdk12.org>
2016-09-13 13:46:21 -0600

    Implement countEvens

--- arrays.rb
+++ arrays.rb
@@ -5,9 +5,24 @@ def haveThree(arr)
       threes += 1
     end
   end
-  
+
     return threes == 3
 end
 
 print haveThree([3, 1, 3, 1, 3])
 print haveThree([3, 4, 3, 4])
+
+
+def countEvens(nums)
+ evens = 0
+ nums.each do |n|
+   if n%2 == 0
+     evens += 1
+   end
+ end
+ return evens
+end
+
+print countEvens([2, 1, 2, 3, 4])
+print countEvens([2, 2, 0])
+print countEvens([1, 3, 5])
