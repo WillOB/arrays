b2995895c4b321a3b05a666e79d88d787e7c3e20
William Patrick O Brien <obrienwp@dcsdk12.org>
2016-09-13 13:53:24 -0600

    Implement sum13

--- arrays.rb
+++ arrays.rb
@@ -26,3 +26,17 @@ end
 print countEvens([2, 1, 2, 3, 4])
 print countEvens([2, 2, 0])
 print countEvens([1, 3, 5])
+
+def sum13(add)
+ sum = 0
+ add.each do |n|
+   if n != 13 && (n - 1) != 13
+     sum + n
+   end
+ end
+ return sum
+end
+
+print sum13([1, 2, 2, 1])
+print sum13([1, 1])
+print sum13([1, 2, 2, 1, 13])
