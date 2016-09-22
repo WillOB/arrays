40121f34a5629296f334aed679ee17b7e2a6e50e
William Patrick O Brien <obrienwp@dcsdk12.org>
2016-09-22 13:29:52 -0600

    Rename to arrays2

--- arrays2.rb
+++ arrays2.rb
@@ -1,17 +1,3 @@
-def haveThree(arr)
-  threes = 0
-  arr.each do |n|
-    if n == 3
-      threes += 1
-    end
-  end
-
-    return threes == 3
-end
-
-print haveThree([3, 1, 3, 1, 3])
-print haveThree([3, 4, 3, 4])
-
 
 def countEvens(nums)
  evens = 0
