b5aa22fb6ed98595a33bd928e4a3ccba47a05590
William Patrick O Brien <obrienwp@dcsdk12.org>
2016-09-22 14:19:44 -0600

    implement reverse3

--- arrays1.rb
+++ arrays1.rb
@@ -1,6 +1,25 @@
 def firstLast6?(arr)
 arr[0] == 6 || arr[-1] == 6
 end
+
 print firstLast6?([1, 2, 6])
 print firstLast6?([6, 1, 2, 3])
 print firstLast6?([13, 6, 1, 2, 3])
+
+
+def commonEnd?(arr1, arr2)
+ arr1[0] == arr2[0] || arr1[-1] == arr2[-1]
+end
+
+print commonEnd?([1, 2, 3], [7, 3])
+print commonEnd?([1, 2, 3], [7, 3, 2])
+print commonEnd?([1, 2, 3], [1, 3])
+
+
+def reverse3(nums)
+  return nums.reverse
+end
+
+print reverse3([1, 2, 3])
+print reverse3([5, 11, 9])
+print reverse3([7, 0, 0])
